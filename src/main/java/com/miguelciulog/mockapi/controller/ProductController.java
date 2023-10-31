d19ffcb0b45eb67dd455c29f1fcc50341cac9fe8
MiguelCiulog <angel.ciulog7@gmail.com>
2023-10-30 22:41:35 -0700

    feat: basic products mapping

---
+++ src/main/java/com/miguelciulog/mockapi/controller/ProductController.java
@@ -0,0 +1,15 @@
+package com.miguelciulog.mockapi.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    @GetMapping()
+    public String error() {
+        return "Hello World!";
+    }
+}
